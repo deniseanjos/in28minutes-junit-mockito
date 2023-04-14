88d5b4e0d438a0e80fca5b4dc4a9c05117751162
Denise Anjos <deniseb.anjos@outlook.com>
2023-04-13 21:57:13 -0300

    Test implementation using JUnit and Stub class

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # in28minutes-junit-mockito
-Repo dedicado aos estudos de testes unitários em Java com Mockito, JUnit e Maven do curso disponível no canal do youtube "in28minutes"
+Repo dedicado aos estudos de testes unitários em Java com Mockito, JUnit e Maven do curso disponível no canal do youtube "in28minutes".
+Vídeo [Mockito Tutorial - Mocking with JUnit and Maven](https://youtu.be/d2KwvXQgQx4)
---
+++ mockito-course/src/test/java/com/in28minutes/business/TodoBusinessImplStubTest.java
@@ -0,0 +1,23 @@
+package com.in28minutes.business;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.in28minutes.data.api.TodoService;
+import com.in28minutes.data.api.TodoServiceStub;
+
+public class TodoBusinessImplStubTest {
+
+	@Test
+	public void testRetrieveTodosRelatedSpring_usingAStub() {
+		TodoService service = new TodoServiceStub();
+		TodoBusinessImpl todoImp = new TodoBusinessImpl(service);
+		List<String> filteredTodos = todoImp.retrieveTodosRelatedToSpring("UserTest");
+		
+		assertEquals(2, filteredTodos.size());
+	}
+
+}
